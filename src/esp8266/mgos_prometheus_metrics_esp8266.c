602d7e24d70ca26b1dc6651f4d2e6665e1a5d780
Pim van Pelt <pim@ipng.nl>
2017-11-21 21:08:07 +0100

    Consistent ordering -- platform first, then wifi metrics

--- src/esp8266/mgos_prometheus_metrics_esp8266.c
+++ src/esp8266/mgos_prometheus_metrics_esp8266.c
@@ -18,11 +18,11 @@ static void metrics_wifi(struct mg_connection *nc) {
 #endif // MGOS_HAVE_WIFI
 
 void metrics_platform(struct mg_connection *nc) {
-#if MGOS_HAVE_WIFI
-  metrics_wifi(nc);
-#endif // MGOS_HAVE_WIFI  
-
   mg_printf(nc, "# HELP esp8266_chip_info ESP8266 Chip Information\r\n");
   mg_printf(nc, "# TYPE esp8266_chip_info gauge\r\n");
   mg_printf(nc, "esp8266_chip_info{sdk=\"%s\",cpu_freq=%u} 1\r\n", system_get_sdk_version(), system_get_cpu_freq());
+
+#if MGOS_HAVE_WIFI
+  metrics_wifi(nc);
+#endif // MGOS_HAVE_WIFI  
 }
